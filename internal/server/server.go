0c2c47bcef41f4ef943075f45140baa5ae6b4cfe
Pao Patarapong <35144104+woonmapao@users.noreply.github.com>
2022-07-06 01:13:25 +0700

    Add starting files

---
+++ internal/server/server.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/websocket/v2"
+)
+
+var (
+	addr = flag.String("addr,":"", os.Getenv("PORT"), "")
+	cert = flag.String("cert", "", "")
+	key = flag.String("key", "", "")
+)
+
+func Run() error {
+	flag.Parse()
+
+	if *addr = ":"{
+		*addr = ":8080"
+	}
+
+	app.Get("/", handlers.Welcome)
+	app.Get("/room/create", handlers.RoomCreate)
+	app.Get("/room/:uuid", handlers.Room)
+	app.Get("/room/:uuid/websocket", )
+	app.Get("/room/:uuid/chat", handlers.RoomChat)
+	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
+	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
+	app.Get("/stream/:ssuid", handlers.Stream)
+	app.Get("/stream/:ssuid/websocket",)
+	app.Get("stream/:ssuid/chat/websocket",)
+	app.Get("/stream/:ssuid/viewer/websocket",)
+}
